60f3691048cd19e8f3efdfc25dda1c7d67f46e92
Chinmoy Kumar Biswas <bangladeshchinmoy@gmail.com>
2025-07-27 22:31:21 +0600

    Update types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -17,3 +17,13 @@ export interface SharedModalProps {
   navigation: boolean;
   direction?: number;
 }
+
+
+export interface ImageProps {
+  id: number;
+  height: string;
+  width: string;
+  public_id: string;
+  format: string;
+  blurDataUrl?: string;
+}
